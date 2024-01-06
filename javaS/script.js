a1e8acacb5e3b97b4875bdcbc8f6af68d1c9efe5
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2024-01-06 08:21:50 -0300

    começo do Js

--- javaS/script.js
+++ javaS/script.js
@@ -1,13 +1,70 @@
-const closeMenu = document.querySelector('.closeMenu')
-const openMenu = document.querySelector('.open')
-const menu=document.querySelector('.menu')
+const openMenu = document.querySelector('.open');
+const closeMenu = document.querySelector('.closeMenu');
+const menu = document.querySelector('.menu');
+const ajuda = document.querySelector('.ajuda');
+const intitucional = document.querySelector('.institucional');
+const iconDownAjuda = document.querySelector('.iconDown_ajuda');
+const iconDownInst = document.querySelector('.iconDown_inst');
 
-closeMenu.addEventListener('click', () => {
-    menu.classList.add('hidden');
-    menu.style.display='none'
-});
+
 
 openMenu.addEventListener('click', () => {
     menu.classList.remove('hidden');
-    menu.style.display='block'
+    setInterval(() => {
+        menu.style.display = 'block'
+    }, 10)
 });
+
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.add('hidden');
+    setTimeout(() => {
+        menu.style.display = 'none';
+    }, 1500);
+});
+
+ajuda.addEventListener('click', () => {
+    const existingIconUp = ajuda.querySelector('.bi-chevron-up');
+    const existingIconDown = ajuda.querySelector('.iconDown_ajuda');
+
+    
+
+    const iconUp = document.createElement('i')
+    iconUp.classList.add("bi")
+    iconUp.classList.add("bi-chevron-up")
+
+
+    if (existingIconUp) {
+        ajuda.removeChild(existingIconUp);
+        ajuda.appendChild(iconDownAjuda);
+
+        document.querySelector('.ajudaOpt').style.display='block';
+    }
+
+    if (existingIconDown) {
+        ajuda.removeChild(existingIconDown);
+        ajuda.appendChild(iconUp);
+
+        document.querySelector('.ajudaOpt').style.diplay = 'none';
+        console.log(ajudaOpt)
+    }
+
+})
+intitucional.addEventListener('click', () => {
+    const iconUp = document.createElement('i')
+    iconUp.classList.add("bi")
+    iconUp.classList.add("bi-chevron-up")
+
+    const existingIconUp = intitucional.querySelector('.bi-chevron-up');
+    const existingIconDown = intitucional.querySelector('.iconDown_inst');
+
+    if (existingIconUp) {
+        intitucional.removeChild(existingIconUp);
+        intitucional.appendChild(iconDown_inst)
+    }
+
+    if (existingIconDown) {
+        intitucional.removeChild(existingIconDown);
+        intitucional.appendChild(iconUp)
+    }
+})
