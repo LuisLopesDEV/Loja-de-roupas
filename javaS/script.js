747ad03d5bc8c531d8b849b3a76e6720f2739b96
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2024-01-07 07:18:26 -0300

    Mudança no footer

--- javaS/script.js
+++ javaS/script.js
@@ -1,18 +1,20 @@
 const openMenu = document.querySelector('.open');
 const closeMenu = document.querySelector('.closeMenu');
 const menu = document.querySelector('.menu');
+
 const ajuda = document.querySelector('.ajuda');
 const intitucional = document.querySelector('.institucional');
-const iconDownAjuda = document.querySelector('.iconDown_ajuda');
-const iconDownInst = document.querySelector('.iconDown_inst');
+const iconDownAjuda = document.querySelector('.iconDownAjuda');
+const iconDownInst = document.querySelector('.iconDownInst');
 
-
+let ajudaOpt = document.querySelector('.ajudaOpt')
+ajudaOpt.style.display = 'none';
+let instOpt = document.querySelector('.instOpt')
+instOpt.style.display = 'none';
 
 openMenu.addEventListener('click', () => {
     menu.classList.remove('hidden');
-    setInterval(() => {
-        menu.style.display = 'block'
-    }, 10)
+    menu.style.display = 'block';
 });
 
 
@@ -23,48 +25,58 @@ closeMenu.addEventListener('click', () => {
     }, 1500);
 });
 
+let primeiroClique = true;
+
 ajuda.addEventListener('click', () => {
     const existingIconUp = ajuda.querySelector('.bi-chevron-up');
-    const existingIconDown = ajuda.querySelector('.iconDown_ajuda');
+    const existingIconDown = ajuda.querySelector('.iconDownAjuda');
 
-    
+    const iconUpA = document.createElement('i')
+    iconUpA.classList.add("bi")
+    iconUpA.classList.add("bi-chevron-up")
 
-    const iconUp = document.createElement('i')
-    iconUp.classList.add("bi")
-    iconUp.classList.add("bi-chevron-up")
-
+    let ajudaOpt = document.querySelector('.ajudaOpt')
+    ajudaOpt.style.display = 'none';
 
     if (existingIconUp) {
         ajuda.removeChild(existingIconUp);
         ajuda.appendChild(iconDownAjuda);
-
-        document.querySelector('.ajudaOpt').style.display='block';
-    }
-
-    if (existingIconDown) {
+    } else {
         ajuda.removeChild(existingIconDown);
-        ajuda.appendChild(iconUp);
-
-        document.querySelector('.ajudaOpt').style.diplay = 'none';
-        console.log(ajudaOpt)
+        ajuda.appendChild(iconUpA);
     }
 
+    if (primeiroClique) {
+        ajudaOpt.style.display = 'block'
+    } else {
+        ajudaOpt.style.display = 'none'
+    }
+    primeiroClique = !primeiroClique;
 })
+
 intitucional.addEventListener('click', () => {
+    const existingIconUp = intitucional.querySelector('.bi-chevron-up');
+    const existingIconDown = intitucional.querySelector('.iconDownInst');
+
     const iconUp = document.createElement('i')
     iconUp.classList.add("bi")
     iconUp.classList.add("bi-chevron-up")
 
-    const existingIconUp = intitucional.querySelector('.bi-chevron-up');
-    const existingIconDown = intitucional.querySelector('.iconDown_inst');
+    let instOpt = document.querySelector('.instOpt')
+    instOpt.style.display = 'none';
 
     if (existingIconUp) {
         intitucional.removeChild(existingIconUp);
-        intitucional.appendChild(iconDown_inst)
+        intitucional.appendChild(iconDownInst);
+    } else {
+        intitucional.removeChild(existingIconDown);
+        intitucional.appendChild(iconUp);
     }
 
-    if (existingIconDown) {
-        intitucional.removeChild(existingIconDown);
-        intitucional.appendChild(iconUp)
+    if (primeiroClique) {
+        instOpt.style.display = 'block'
+    } else {
+        instOpt.style.display = 'none'
     }
+    primeiroClique = !primeiroClique;
 })
